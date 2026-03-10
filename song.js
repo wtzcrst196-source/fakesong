ffc49a7ff292e8d6d1a2056bc7e1837d2a065441
witz <wtzcrst196@gmail.com>
2026-03-10 13:04:35 -0400

    Update song.js

--- song.js
+++ song.js
@@ -1,6 +1,26 @@
-const songs=[
-{name:"Song 1",src:"songs/song1.mp3"},
-{name:"Song 2",src:"songs/song2.mp3"},
-{name:"Song 3",src:"songs/song3.mp3"},
-{name:"Next door",src:"songs/song4.mp3"}
+const songs = [
+{
+name: "Consume",
+artist: "Artist 1",
+src: "songs/song1.mp3",
+cover: "images/photo-1.jpg"
+},
+{
+name: "Song 2",
+artist: "Artist 2",
+src: "songs/song2.mp3",
+cover: "images/photo-2.jpg"
+},
+{
+name: "Song 3",
+artist: "Artist 3",
+src: "songs/song3.mp3",
+cover: "images/photo-3.jpg"
+},
+{
+name: "Next door",
+artist: "Artist 4",
+src: "songs/song4.mp3",
+cover: "images/photo-4.jpg"
+}
 ]
