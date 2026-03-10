03d482f279d98f12ecf1c044ed0bef0fb5da0a10
witz <wtzcrst196@gmail.com>
2026-03-10 13:27:36 -0400

    Update song.js

--- song.js
+++ song.js
@@ -1,62 +1,26 @@
-let songs = [
-    {
-        name:'7th Floor Tango - Silent Partner',
-        path:'music/song1.mp3',
-        artist:'Artist Y01',
-        image:'images/7th Floor Tango - Silent Partner.jpg'
-    },
-    {
-        name:'Arp Bounce - Geographer',
-        path:'music/song2.mp3',
-        artist:'Artist Y01',
-        image:'images/Arp Bounce - Geographer.jpg'
-    },
-    {
-        name:'BLAAM - Gunnar Olsen HipHop',
-        path:'music/song3.mp3',
-        artist:'Artist Y01',
-        image:'images/BLAAM - Gunnar Olsen HipHop.jpg'
-    },
-    {
-        name:'Burner - Gunnar Olsen',
-        path:'music/song4.mp3',
-        artist:'Artist Y01',
-        image:'images/Burner - Gunnar Olsen.jpg'
-    },
-    {
-        name:'Cielo - Huma-Huma',
-        path:'music/Cielo - Huma-Huma.mp3',
-        artist:'Artist Y01',
-        image:'images/Cielo - Huma-Huma.jpg'
-    },
-    {
-        name:'Circular - Gunnar Olsen',
-        path:'music/Circular - Gunnar Olsen.mp3',
-        artist:'Artist Y01',
-        image:'images/Circular - Gunnar Olsen.jpg'
-    },
-    {
-        name:'Dat Step - Gunnar Olsen',
-        path:'music/Dat Step - Gunnar Olsen.mp3',
-        artist:'Artist Y01',
-        image:'images/Dat Step - Gunnar Olsen.jpg'
-    },
-    {
-        name:'Detour - Gunnar Olsen',
-        path:'music/Detour - Gunnar Olsen.mp3',
-        artist:'Artist Y01',
-        image:'images/Detour - Gunnar Olsen.jpg'
-    },
-    {
-        name:'Far Away - MK2',
-        path:'music/Far Away - MK2.mp3',
-        artist:'Artist Y01',
-        image:'images/Far Away - MK2.jpg'
-    },
-    {
-        name:'Hooky with Sloane - Bird Creek',
-        path:'music/Hooky with Sloane - Bird Creek.mp3',
-        artist:'Artist Y01',
-        image:'images/Hooky with Sloane - Bird Creek.jpg'
-    },
-];
+const songs = [
+{
+name: "Song 1",
+artist: "Artist 1",
+src: "songs/song1.mp3",
+cover: "images/photo-1.jpg"
+},
+{
+name: "Song 2",
+artist: "Artist 2",
+src: "songs/song2.mp3",
+cover: "images/photo-2.jpg"
+},
+{
+name: "Song 3",
+artist: "Artist 3",
+src: "songs/song3.mp3",
+cover: "images/photo-3.jpg"
+},
+{
+name: "Next door",
+artist: "Artist 4",
+src: "songs/song4.mp3",
+cover: "images/photo-4.jpg"
+}
+]
