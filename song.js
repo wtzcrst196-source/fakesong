35ff0ce2293f81a77cfc1ee5ff2338a261ac3554
witz <wtzcrst196@gmail.com>
2026-03-09 20:39:44 -0400

    Add initial song list to song.js

---
+++ song.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: "Song 1",
+        artist: "Artist 1",
+        path: "music/song1.mp3",
+        image: "images/photo-1.jpg"
+    },
+    {
+        name: "Song 2",
+        artist: "Artist 2",
+        path: "music/song2.mp3",
+        image: "images/photo-2.jpg"
+    },
+    {
+        name: "Song 3",
+        artist: "Artist 3",
+        path: "music/song3.mp3",
+        image: "images/photo-3.jpg"
+    }
+];
